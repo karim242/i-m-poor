49abb0dc58b5681071b7f5a379ee0223ac7c2569
karim242 <karimelsa38@gmail.com>
2022-03-19 21:04:59 +0200

    add file via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I AM POOR '),),
+        backgroundColor: Colors.blueGrey[900],),
+        body: Image(
+          image: AssetImage('Image/212.jpg'),
+        ),
+      ),
+    ),
+  );
+}
